d61e9f8fdd0157a68ff91809ac65596fb1fd37f1
nurcahyani <tjah.a5@gmail.com>
2021-06-01 18:12:19 +0700

    Update config.php

--- src/sikap/lib/config.php
+++ src/sikap/lib/config.php
@@ -3,7 +3,7 @@
 	$username 	= "root";
 	$password 	= "";
 	$dbname 	= "sikap";
-	$base_url   = "http://localhost/interop/src/sikap/";
+	$base_url   = "http://localhost/sikap/";
 
 	try {
 	    $conn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
