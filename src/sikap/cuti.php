7c3199f999206b40a436610a5c4bb6987795cce6
Agung RB <agungrbudiman@gmail.com>
2021-06-01 22:32:03 +0700

    add form pengajuan cuti

--- src/sikap/cuti.php
+++ src/sikap/cuti.php
@@ -18,14 +18,63 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="white-box">
-                            <h3 class="box-title m-b-0">Add Data</h3><br>
+                            <h3 class="box-title m-b-0">Pengajuan Cuti</h3><br>
                             <div class="row">
                                 <div class="col-sm-12 col-xs-12">
                                     <form method="post" action="data">
                                         <div class="form-group">
-                                            <label>Nama</label>
-                                            <input type="text" name="nama" class="form-control">
-                                        </div>                                
+                                            <div class="dropdown">
+                                              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                Jenis Cuti
+                                                <span class="caret"></span>
+                                              </button>
+                                              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                                <li><a href="#">Cuti alasan penting</a></li>
+                                                <li><a href="#">Cuti mengurus warisan</a></li>
+                                                <li><a href="#">Cuti tahunan</a></li>
+                                              </ul>
+                                            </div> 
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Saldo Cuti</label>
+                                            <div class="input-group">
+                                                <input type="number" name="saldo-cuti" class="form-control" aria-describedby="basic-addon2" value="">
+                                                <span class="input-group-addon" id="basic-addon2">hari</span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Lama Cuti</label>
+                                            <div class="input-group">
+                                                <input type="number" name="saldo-cuti" class="form-control" aria-describedby="basic-addon2" value="">
+                                                <span class="input-group-addon" id="basic-addon2">hari</span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="container">
+                                                <div class="row">
+                                                    <div class="col-xs-6">
+                                                        <label>Mulai Cuti</label>
+                                                        <div class="input-group">
+                                                            <input type="date" name="saldo-cuti" class="form-control" value="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-6">
+                                                        <label>Akhir Cuti</label>
+                                                        <div class="input-group">
+                                                            <input type="date" name="saldo-cuti" class="form-control" value="">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Alamat</label>
+                                                <textarea rows="5" name="saldo-cuti" class="form-control" value="" placeholder="Text input" style="min-width: 100%"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Keterangan</label>
+                                                <textarea rows="5" name="saldo-cuti" class="form-control" value="" placeholder="Text input" style="min-width: 100%"></textarea>
+                                        </div>                              
                                         <button type="submit" name="add" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
                                         <a href="data" class="btn btn-inverse waves-effect waves-light">Cancel</a>
                                     </form>
