26acff2c68fe22cf9cda7d27c8e3d6ef64de6552
Agung RB <agungrbudiman@gmail.com>
2021-06-03 08:17:18 +0700

    ajax cuti

--- src/sikap/cuti-add.php
+++ src/sikap/cuti-add.php
@@ -1,6 +1,39 @@
 <!-- ============================================================== -->
 <!-- Page Content -->
 <!-- ============================================================== -->
+<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="tambahkategori">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-body">
+            <div id="alert-tambahkategori" class="alert alert-success hidden" role="alert">
+                <strong>Berhasil!</strong>
+            </div>
+            <label>Kategori Cuti</label>
+            <input id="inputkategori" type="text" name="kategori" class="form-control" value="" placeholder="Cuti tahunan">
+        </div>
+        <div class="modal-footer">
+            <a href="cuti-add"><button id="btn-return" type="button" class="btn btn-secondary hidden">Return</button></a>
+            <button id="btn-close" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button id="btn-tambahkategori" type="button" class="btn btn-primary">Save</button>
+          </div>
+        </div>
+  </div>
+</div>
+<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="statuspengajuan">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h2 class="modal-title">Berhasil!</h2>
+        </div>
+      <div class="modal-body">
+        Pengajuan cuti anda berhasil disimpan
+      </div>
+      <div class="modal-footer">
+        <a href="cuti"><button type="button" class="btn btn-primary">Kembali</button></a>
+      </div>
+    </div>
+  </div>
+</div>
 <div id="page-wrapper">
     <div class="container-fluid">
         <div class="row bg-title">
@@ -21,11 +54,10 @@
                     <h3 class="box-title m-b-0">Pengajuan Cuti</h3><br>
                     <div class="row">
                         <div class="col-sm-12 col-xs-12">
-                            <form method="post" action="cuti-add">
                                 <div class="form-group">
                                     <label>Kategori Cuti</label>
                                     <div class="input-group">
-                                        <select name="jenis_cuti" class="form-control">
+                                        <select id="jenis_cuti" name="jenis_cuti" class="form-control">
                                             <?php
                                             $sql = "SELECT * FROM jenis_cuti ORDER BY id DESC";
                                             $result = $conn->query($sql);
@@ -37,54 +69,39 @@
                                         <button type="button" class="btn btn-link" aria-label="Left Align" style="padding-left: 0;" data-toggle="modal" data-target="#tambahkategori">tambah kategori</button>
                                   </div>
                                 </div>
-                                <div class="modal fade" id="tambahkategori" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                  <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                      <div class="modal-body">
-                                        <label>Kategori Cuti</label>
-                                        <input type="text" name="kategori" class="form-control" value="" placeholder="Cuti tahunan">
-                                      </div>
-                                      <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="submit" class="btn btn-primary">Save</button>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
                                 <div class="form-group">
                                     <label>Durasi Cuti</label>
                                     <div class="input-group">
-                                        <input type="number" name="durasi" class="form-control" value="" placeholder="1">
+                                        <input id="durasi" type="number" name="durasi" class="form-control" value="" placeholder="1">
                                         <span class="input-group-addon" id="basic-addon2">hari</span>
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Mulai Cuti</label>
                                     <div class="input-group">
-                                        <input type="date" name="cuti_start" class="form-control" value="<?php echo date('Y-m-d'); ?>">
+                                        <input id="cuti_start" type="date" name="cuti_start" class="form-control" value="<?php echo date('Y-m-d'); ?>">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Akhir Cuti</label>
                                     <div class="input-group">
-                                        <input type="date" name="cuti_end" class="form-control" value="<?php echo date('Y-m-d'); ?>">
+                                        <input id="cuti_end" type="date" name="cuti_end" class="form-control" value="<?php echo date('Y-m-d'); ?>">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Alamat</label>
-                                        <textarea rows="5" name="alamat" class="form-control" value="" placeholder="Jl.Garuda No 17" style="min-width: 100%"></textarea>
+                                        <textarea id="alamat" rows="5" name="alamat" class="form-control" value="" placeholder="Jl.Garuda No 17" style="min-width: 100%"></textarea>
                                 </div>
                                 <div class="form-group">
                                     <label>Keterangan</label>
-                                        <textarea rows="5" name="keterangan" class="form-control" value="" placeholder="Keterangan tambahan jika ada" style="min-width: 100%"></textarea>
+                                        <textarea id="keterangan" rows="5" name="keterangan" class="form-control" value="" placeholder="Keterangan tambahan jika ada" style="min-width: 100%"></textarea>
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputFile">File Pendukung</label>
                                     <input type="file" id="exampleInputFile">
                                 </div>                              
-                                <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
+                                <button id="btn-pengajuan" type="button" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
                                 <a href="cuti" class="btn btn-inverse waves-effect waves-light">Cancel</a>
-                            </form>
                         </div>
                     </div>
                 </div>
@@ -113,14 +130,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sql = "INSERT INTO jenis_cuti VALUES('0','$kategori')";
     }
     $query = $conn->query($sql);
-    if($query === TRUE) {
-        echo '<div class="alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <strong>Success!</strong> Proses berhasil.
-        </div>';
-    }
-    else {
+    if ($query == FALSE) {
 
     }
 }
 ?>
+<script>
+$( document ).ready(function() {
+    $("#btn-tambahkategori").click(function(){
+        $.post("cuti-add",
+          {
+            kategori: $("#inputkategori").val()
+          },
+        function(data, status){
+            $("#alert-tambahkategori").removeClass("hidden");
+            $("#btn-tambahkategori").prop("disabled", true);
+            $("#btn-close").addClass("hidden");
+            $("#btn-return").removeClass("hidden");
+        });
+    });
+    $("#btn-pengajuan").click(function(){
+        $.post("cuti-add",
+          {
+            jenis_cuti: $("#jenis_cuti").val(),
+            durasi: $("#durasi").val(),
+            cuti_start: $("#cuti_start").val(),
+            cuti_end: $("#cuti_end").val(),
+            alamat: $("#alamat").val(),
+            keterangan: $("#keterangan").val()
+          },
+        function(data, status){
+            $("#statuspengajuan").modal('show');
+        });
+    });
+});
+</script>
